c0c9c8d5a7a6522ba299031cec4d38bc53ca5390
Nikola Zrnic <zrnic.nikola@outlook.com>
2025-02-21 11:25:35 +0100

    Added Kapitel 9 questions. Added Kapitel 9 button

--- src/elements/Questions.jsx
+++ src/elements/Questions.jsx
@@ -109,6 +109,26 @@ function Questions() {
       })
       .catch(error => console.error("Error fetching data:", error))
   }
+  const fetchKapitelNineQuestions = () => {
+    fetch("./questions.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then(response => response.json())
+      .then(myJson => {
+        const filteredQuestions = myJson.questions.filter(
+          question => question.Kapitel === 9
+        )
+        setData(filteredQuestions.map((q, i) => ({ ...q, originalIndex: i })))
+        setRandomizedQuestions(
+          filteredQuestions.map((q, i) => ({ ...q, originalIndex: i }))
+        )
+        setShowAll(true) // Show all filtered questions
+      })
+      .catch(error => console.error("Error fetching data:", error))
+  }
 
   const handleCheckboxChange = (questionIndex, answerIndex, e) => {
     const checked = e?.target?.checked ?? e // Fallback to `e` if it's directly a boolean
@@ -209,6 +229,9 @@ function Questions() {
         </button>
         <button onClick={fetchKapitelEightQuestions} className="custom-button">
           Get Kapitel 8 Questions
+        </button>
+        <button onClick={fetchKapitelNineQuestions} className="custom-button">
+          Get Kapitel 9 Questions
         </button>
       </div>
       <div className="cards-container">
